783a5bdb5c42df320efa02006f63069543892447
Dzaky Badawi <dzaky.badawi@gmail.com>
2022-04-14 14:51:44 +0700

    feat: add useRef content

--- src/pages/UseRef/UseRef.jsx
+++ src/pages/UseRef/UseRef.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useRef, useState } from 'react'
 
 const UseRef = () => {
+  const [name, setName] = useState('')
+  const inputRef = useRef(null)
+  const nameSubmit = (e) => {
+    e.preventDefault()
+    setName(inputRef.current?.value)
+    inputRef.current.value = ''
+    inputRef.current.focus()
+  }
+
+  // useRef is used for to make pointer or reference into DOM element
+
   return (
-    <div>UseRef</div>
+    <>
+      <h2>UseRef</h2>
+      <br />
+      <form onSubmit={nameSubmit}>
+        <input placeholder='Type your name...' ref={inputRef} name='name' />
+        <button type='submit'>Change Name</button>
+      </form>
+      <br />
+      <h2>Hello, {name || "No Name"}</h2>
+    </>
   )
 }
 
