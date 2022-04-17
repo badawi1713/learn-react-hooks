d3b0e825c086ae716f504d8c5b23acaaafc1f6fd
Dzaky Badawi <dzaky.badawi@gmail.com>
2022-04-17 14:07:37 +0700

    feat: add useLayoutEffect content

--- src/pages/UseLayoutEffect/UseLayoutEffect.jsx
+++ src/pages/UseLayoutEffect/UseLayoutEffect.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 
 const UseLayoutEffect = () => {
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.value = "HELLO"
+    console.log('useEffect', inputRef.current.value)
+  }, [])
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect", inputRef.current.value)
+  }, [])
+
+  // the difference between useLayoutEffect and UseEffect is
+  // useLayoutEffect always called first when page is rendering
+  // very useful if you want to change your layout before showing data or function
+
   return (
-    <div>UseLayoutEffect</div>
+    <>
+      <h1>UseLayoutEffect</h1>
+      <br />
+      <input ref={inputRef} value="DZAKY" onChange={e => console.log(e)} />
+    </>
   )
 }
 
