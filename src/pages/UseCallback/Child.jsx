794e66a9850964ed7243b7ad526c394b031bd743
Dzaky Badawi <dzaky.badawi@gmail.com>
2022-04-18 14:44:27 +0700

    feat: add useCallback hooks content

---
+++ src/pages/UseCallback/Child.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+const Child = ({ returnComment }) => {
+
+    useEffect(() => {
+        console.log('FUNCTION WAS CALLED')
+    }, [returnComment])
+    return (
+        <div>{returnComment()}</div>
+    )
+}
+
+export default Child
--- src/pages/UseCallback/UserCallback.jsx
+++ src/pages/UseCallback/UserCallback.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useCallback } from "react";
+import { useState } from "react";
+import Child from "./Child";
 
 const UserCallback = () => {
+  const [data, setData] = useState('Hello Dzaky!')
+  const [toggle, setToggle] = useState(false)
+
+  // useCallback hooks its just like useMemo, if useMemo is only use to get access of the value, useCallback is used to get the function!
+
+  const returnComment = useCallback(() => {
+    return data;
+  }, [data])
   return (
-    <div>UserCallback</div>
+    <>
+      <h1>UseCallback</h1>
+      <Child returnComment={returnComment} />
+      <br />
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <h4>toggle</h4>}
+    </>
   )
 }
 
